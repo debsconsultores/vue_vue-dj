59baa52655a5171742fac7859c2b2bebfe3431d6
debsdaniel <debs.foros@gmail.com>
2020-07-26 02:28:14 -0600

    Consumir API con Axios

---
+++ src/components/pixabay/ServicioAPI.js
@@ -0,0 +1,16 @@
+import api from "./axios";
+
+const API_KEY=process.env.VUE_APP_KEY;
+
+class ServicioAPI
+{
+    async getImagenes(data,pg=1){
+        const encode = encodeURIComponent(data);
+        const cadena = `?key=${API_KEY}&q=${encode}&lang=es&page${pg}`;
+        const r = await api.get(cadena)
+        console.log(r);
+        return r.data;
+    }
+}
+
+export default new ServicioAPI();
