3165c213c53498bf7f3fc85b7a8187ac2c72e609
debsdaniel <debs.foros@gmail.com>
2020-07-26 01:48:43 -0600

    Mejorando Componente Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,12 +6,42 @@
                 <img src="../assets/Logo-VueDJ.png" alt="" width="100%" height="auto" rounded="circle">
             </b-col>
         </b-row>
+        <b-row>
+            <b-col v-for="red in redes" :key="red.red" class="text-center">
+                <b-link :href="red.url" target="_blank">
+                    <b-alert :variant="red.color" show>
+                        {{red.red}}
+                    </b-alert>
+                </b-link>
+            </b-col>
+        </b-row>
     </b-container>
 </template>
 
 <script>
 export default {
-    name:"Home"
+    name:"Home",
+    data(){
+        return{
+            redes:[
+                {
+                    red:"FB/DEBSConsultores",
+                    url:"https://facebook.com/DEBSConsultores",
+                    color:"info"
+                },
+                {
+                    red:"TW/DEBSConsultores",
+                    url:"https://twitter.com/DEBSConsultores",
+                    color:"warning"
+                },
+                {
+                    red:"YT/DEBSDaniel03",
+                    url:"https://youtube.com/DEBSDaniel03",
+                    color:"danger"
+                }
+            ]
+        }
+    }
     
 }
 </script>
