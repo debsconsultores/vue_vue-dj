946abbe1b394e9ec527f8d5ea5aab7c3ef2f70c2
debsdaniel <debs.foros@gmail.com>
2020-07-26 04:08:46 -0600

    Ventana Modal - desde Componente

--- src/components/pixabay/Imagen.vue
+++ src/components/pixabay/Imagen.vue
@@ -2,27 +2,38 @@
     <div>
         <b-img :id="img.id" :src="img.previewURL" thumbnail fluid rounded
         v-b-popover.hover.top="img.tags" title="Curso Vue + Django: Etiqueta"
-        @click="showModal"
+        @click="mostrarModal=true"
         ></b-img>
 
-        <b-modal :ref="img.id" hide-footer size="xl" :title="img.tags">
+        <!-- <b-modal :ref="img.id" hide-footer size="xl" :title="img.tags">
             <div class="d-block text-center">
                 <b-img-lazy top :id="img.id" :src="img.largeImageURL" :alt="img.tags" fluid rounded v-b-popover.focus="img.tags"
         ></b-img-lazy>
             </div>
-        </b-modal>
+        </b-modal> -->
+        <Modal :mostrarModal="mostrarModal" :img="img" />
     </div>
 </template>
 
 <script>
+import Modal from "./Modal"
+
 export default {
     name:"Imagen",
     props:["img"],
+    components:{
+        Modal,
+    },
     methods:{
         showModal(){
             this.$refs[this.img.id].show();
         }
-    }    
+    },
+    data(){
+        return{
+            mostrarModal:false
+        }
+    }
 }
 </script>
 
---
+++ src/components/pixabay/Modal.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <b-modal v-model="mostrar" hide-footer size="xl" :title="img.tags"
+        scrollable>
+            <div class="d-block text-center">
+                <b-img-lazy top :id="img.id" :src="img.largeImageURL" :alt="img.tags" fluid rounded v-b-popover.focus="img.tags"
+        ></b-img-lazy>
+            </div>
+        </b-modal>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Modal",
+    props:["img","mostrarModal"],
+    computed:{
+        mostrar:{
+            get(){
+                return this.mostrarModal
+            },
+            set(valor){
+                return valor
+            }
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
