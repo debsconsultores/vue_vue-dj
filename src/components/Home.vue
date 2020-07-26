2650045560e0b3fbe575ea76c7c39fcf4f4502ad
debsdaniel <debs.foros@gmail.com>
2020-07-26 01:33:58 -0600

    Componente Home.vue

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <b-container>
+        <b-row>
+            <b-col>
+                <h1>Bienvenidos al Curso de Vue + Django <small>x Daniel Bojorge</small></h1>
+                <img src="../assets/Logo-VueDJ.png" alt="" width="100%" height="auto" rounded="circle">
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+    name:"Home"
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+import BootstrapVue from "bootstrap-vue"
+
+Vue.use(BootstrapVue);
+
 Vue.config.productionTip = false
 
 new Vue({
