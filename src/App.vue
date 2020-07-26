177b0c4a490899d961fbade4b44c2820a869ecfe
debsdaniel <debs.foros@gmail.com>
2020-07-26 02:42:21 -0600

    Estructura Componente Pixabay.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <Home></Home>
+    <Home v-if="false"></Home>
+    <Pixabay></Pixabay>
   </div>
 </template>
 
 <script>
-import Home from "./components/Home"
+import Home from "./components/Home";
+import Pixabay from "./components/pixabay/Pixabay";
 
 export default {
   name: 'App',
   components: {
     Home,
+    Pixabay,
   }
 }
 </script>
---
+++ src/components/pixabay/Pixabay.vue
@@ -0,0 +1,38 @@
+<template>
+    <b-container>
+        <b-row>
+            <b-col>
+
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+import ServicioAPI from "./ServicioAPI";
+
+export default {
+    name:"Pixabay",
+    data(){
+        return{
+            pixaImagenes:[],
+            buscar:""
+        }
+    },
+    methods:{
+        async buscarImagenes(){
+            const consulta = await ServicioAPI.getImagenes(this.buscar);
+            this.pixaImagenes = consulta.hits;
+            console.log(consulta);
+        }
+    },
+    mounted(){
+        this.buscarImagenes();
+    }
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
