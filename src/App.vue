712820794f8492b940446b5ae022503521a51efa
debsdaniel <debs.foros@gmail.com>
2020-08-02 22:09:01 -0600

    06-006 Creando la barra de menú

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
 <template>
-  <div id="app">
-    <!-- <Home v-if="false"></Home>
-    <Pixabay /> -->
-    <router-link to="/">Home</router-link> | 
-    <router-link to="/pixa">Imágenes</router-link>
+  <v-app>
+    <v-navigation-drawer permanent expand-on-hover>
+      <v-list>
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img src="./assets/Logo-VueDJ.png"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
 
-    <router-view></router-view>
-  </div>
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="title">Curso Vue + Django</v-list-item-title>
+            <v-list-item-subtitle>Daniel Bojorge</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list nav dense>
+        <div v-for="m in menu" :key="m.nombre">
+          <router-link :to='m.ruta'>
+            <v-list-item link>
+                <v-list-item-icon><v-icon>{{m.icono}}</v-icon></v-list-item-icon>
+                <v-list-item-title>{{m.nombre}}</v-list-item-title>
+            </v-list-item>
+          </router-link>
+        </div>
+      </v-list>
+    </v-navigation-drawer>
+     <router-view></router-view>
+  </v-app>
 </template>
 
 <script>
@@ -18,9 +42,20 @@ export default {
   components: {
     // Home,
     // Pixabay,
+  },
+  data(){
+    return {
+      menu:[
+        {ruta:"/",nombre:"Inicio",icono:"mdi-folder"},
+        {ruta:"/pixa",nombre:"Imágenes",icono:"mdi-folder"}
+      ]
+    }
   }
 }
 </script>
 
 <style>
+a:link {
+  text-decoration: none;
+}
 </style>
