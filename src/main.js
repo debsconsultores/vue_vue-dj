88b573717a265feea8d7640e4678c4303b705184
debsdaniel <debs.foros@gmail.com>
2020-08-02 21:35:58 -0600

    06-005 Instalando y Configurando vuetify

--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import vuetify from './plugins/vuetify';
+import Vuetify from 'vuetify'
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 import BootstrapVue from "bootstrap-vue"
 import router from "./router"
 
 
+
+Vue.use(Vuetify)
 Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
