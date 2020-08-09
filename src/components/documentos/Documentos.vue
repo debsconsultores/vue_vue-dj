8171655ae73e4a171d9c8da230e32214edac3426
debsdaniel <debs.foros@gmail.com>
2020-08-09 00:40:52 -0600

    08-011 Componente Documentos

---
+++ src/components/documentos/Documentos.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+    </div>    
+</template>
+
+<script>
+export default {
+    name:"Documentos",
+    components:{
+
+    },
+    data: () => {
+        return {
+            nuevo: {
+                id:-1,
+                nombre:"Nuevo Documento",
+                expira:"",
+                alerta1y:true,
+                alerta6m: true,
+				alerta3m: true,
+				alerta1m: true
+            },
+            item:[]
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
