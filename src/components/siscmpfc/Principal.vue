fcb183a7211e64c3094c5bb584642c5076a910cc
debsdaniel <debs.foros@gmail.com>
2020-09-14 23:39:50 -0600

    09-005 Tabs en Componente Principal

--- src/components/siscmpfc/Principal.vue
+++ src/components/siscmpfc/Principal.vue
@@ -3,6 +3,15 @@
         <v-card-title>
             <h1 class="font-weight-bold display-6 color-texto">Sistema de Compra y Facturación <small>Curso x Daniel Bojorge</small></h1>
         </v-card-title>
+        <v-tabs>
+            <v-tab>Inventario</v-tab>
+            <v-tab>Compra</v-tab>
+            <v-tab>Facturación</v-tab>
+
+            <v-tab-item>Detalle Módulo Inventario</v-tab-item>
+            <v-tab-item>Detalle Módulo Compras</v-tab-item>
+            <v-tab-item>Detalle Módulo Facturación</v-tab-item>
+        </v-tabs>
     </v-card>
 </template>
 
