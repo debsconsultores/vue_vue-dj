42c688efc8a5bfca89c965a70c2cb4cd3ec2cb06
debsdaniel <debs.foros@gmail.com>
2020-09-19 23:48:36 -0600

    09-011 Cargando Categorías en una tabla

--- src/components/siscmpfc/inv/Categoria.vue
+++ src/components/siscmpfc/inv/Categoria.vue
@@ -1,10 +1,33 @@
 <template>
     <v-app>
+        <v-container>
             <v-row>
                 <v-col>
-                    {{items}}
+                    <v-data-table
+                    dense
+                    :headers="headers"
+                    :items="items"
+                    :search="search"
+                    class="elevation-1"
+                    :loading="loading"
+                    loading-text="Cargando..."
+                    >
+                    <template v-slot:top>
+                        <v-toolbar flat color="white">
+                        <v-toolbar-title>Categorías</v-toolbar-title>
+                        <v-divider class="mx-4" inset vertical></v-divider>
+                        <v-text-field  v-model="search" append-icon="mdi-magnify" label="Buscar"></v-text-field> 
+                        <v-spacer></v-spacer>
+                        <v-btn color="warning" dark class="mb-2" @click="iniciar" ><v-icon>cached</v-icon></v-btn>
+                        </v-toolbar>
+                    </template>
+                    <template v-slot:no-data>
+                        <v-btn color="primary" @click="iniciar">Reiniciar</v-btn>
+                    </template>
+                    </v-data-table>
                 </v-col>
-            </v-row>      
+            </v-row> 
+        </v-container>     
     </v-app>
 </template>
 
@@ -16,13 +39,31 @@ export default {
     data(){
         return {
             items: [],
-            api : new ApiInv
+            api : new ApiInv,
+            loading:false,
+            search : "",
+            headers: [
+                {text:"ID",value:"id"},
+                {
+                    text:"Descripción",
+                    align: 'start',
+                    sortable: true,
+                    value: 'descripcion'
+                }
+            ]
         }
     },
     methods:{
         async iniciar(){
-            let r = await this.api.getCategorias()
-            this.items = r
+            try {
+                this.loading = true
+                let r = await this.api.getCategorias()
+                this.items = r   
+            } catch (error) {
+                alert(error)
+            }finally{
+                this.loading = false
+            }
         }
     },
     created(){
