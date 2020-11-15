30ca74ab58f5fcb128c78a9f544696e2c573ca8f
debsdaniel <debs.foros@gmail.com>
2020-11-14 23:18:18 -0600

    11-013 Consumir Api Compra

--- src/components/siscmpfc/cmp/ApiCmp.js
+++ src/components/siscmpfc/cmp/ApiCmp.js
@@ -1,6 +1,9 @@
 
 import Api from "../Api"
 
+const nombreEnc = "compras";
+const nombreDet = "compras-detalle";
+
 export class ApiCmp extends Api{
     constructor(){
         super()
@@ -16,5 +19,21 @@ export class ApiCmp extends Api{
 
     async delProveedor(id){
         return await super.delete("proveedor",id)
+    }
+
+    async get(id){
+        return await super.get(nombreEnc,id)
+    }
+
+    async guardarEncabezado(obj){
+        return await super.save(nombreEnc,obj);
+    }
+
+    async guardarDetalle(obj){
+        return await super.save(nombreDet,obj);
+    }
+
+    async borrarDetalle(id){
+        return await super.delete(nombreDet,id);
     }
 }
