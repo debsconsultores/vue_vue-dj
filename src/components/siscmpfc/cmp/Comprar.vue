f9a838aab99949ba907d7da6a98d5a4ba714bea8
debsdaniel <debs.foros@gmail.com>
2020-11-27 23:02:49 -0600

    11-019 Deshabilitar Controles

--- src/components/siscmpfc/cmp/Comprar.vue
+++ src/components/siscmpfc/cmp/Comprar.vue
@@ -24,7 +24,8 @@
                                 v-on="on"
                             ></v-text-field>
                             </template>
-                            <v-date-picker v-model="editedEnc.fecha" scrollable color="success" locale="es">
+                            <v-date-picker v-model="editedEnc.fecha" scrollable color="success" locale="es"
+                            :disabled="editedEnc.id!=-1">
                             <v-spacer></v-spacer>
                             <v-btn text color="primary" @click="dialogFecha = false">Cancel</v-btn>
                             <v-btn text color="primary" @click="$refs.dialog.save(editedEnc.fecha)">OK</v-btn>
@@ -41,6 +42,7 @@
                         return-object
                         prepend-icon="mdi-city"
                         :rules="textRules"
+                        :disabled="editedEnc.id!=-1"
                         ></v-autocomplete>
                     </v-col>
                 </v-row>
