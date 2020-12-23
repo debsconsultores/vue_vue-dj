ba988c494b294d7894f7c158a201870aa0c7b6c9
debsdaniel <debs.foros@gmail.com>
2020-12-22 22:48:42 -0600

    13-004 Componente Cliente - Scripts

---
+++ src/components/siscmpfc/fac/Clientes.vue
@@ -0,0 +1,76 @@
+<template>
+    
+</template>
+
+<script>
+import { ApiFac } from "./ApiFac";
+import { BIcon } from "bootstrap-vue";
+
+export default {
+    name:"Cliente",
+	components: {
+        BIcon
+    },
+    data() {
+        return {
+            modalShow: false,
+            loading: false,
+            filter: "",
+            api: new ApiFac(),
+            fields: [
+                { key: "id", label: "ID", sortable: true},
+                {
+                key: "nombre",
+                label: "Nombres",
+                sortable: true
+                },
+                {
+                key: "telefono",
+                label: "Teléfono",
+                sortable: true
+                },
+                {
+                key: "email",
+                label: "E-Mail",
+                sortable: true
+                },
+                {
+                key: "estado",
+                label: "Activo?",
+                sortable: true
+                },
+                { key: "acciones", label: "Acciones", sortable: false }
+            ],
+            items: [],
+            cliente:{
+                id:-1,
+                nombre:"",
+                telefono:"",
+                email:"",
+                estado:true
+            }
+        };
+    },
+    created(){
+        this.iniciar()
+    },
+    computed:{},
+    methods:{
+        async iniciar(){
+            try {
+                this.loading = true
+                const r = await this.api.getCliente()
+                this.items = r
+            } catch (error) {
+                alert(error)
+            } finally {
+                this.loading = false
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
